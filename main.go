8f3aaa73eaf3a694cd1289efdcbcacd6a9429049
Nathan N <Nitharios@users.noreply.github.com>
2019-03-05 21:17:50 -0800

    updated import paths

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"os"
 
 	_ "github.com/joho/godotenv/autoload"
+	router "github.com/nitharios/bastion/api"
 )
 
 func main() {
 	PORT := os.Getenv("PORT")
 
-	router := NewRouter()
+	router := router.New()
 
-	log.Fatal(http.ListenAndServe(PORT, router))
+	log.Fatal(http.ListenAndServe(":"+PORT, router))
 }
